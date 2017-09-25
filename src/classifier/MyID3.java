8aacae277842d8cf63de6e043ebbcd9f86b5f455
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2017-09-26 00:25:59 +0700

    Add new test options, and add select classifier.

---
+++ src/classifier/MyID3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classifier;
+
+import weka.classifiers.AbstractClassifier;
+import weka.core.Instances;
+
+/**
+ *
+ * @author BerviantoLeoPratama
+ */
+public class MyID3 extends AbstractClassifier {
+
+    @Override
+    public void buildClassifier(Instances i) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/weka_decisiontree/Weka_decisionTree.java
+++ src/weka_decisiontree/Weka_decisionTree.java
@@ -5,82 +5,119 @@
  */
 package weka_decisiontree;
 
+import classifier.MyID3;
+import classifier.MyJ48;
 import java.util.Random;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import weka.ID3;
 import weka.classifiers.Classifier;
 
 import weka.core.Instances;
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
-import weka.core.DenseInstance;
 import weka.core.Instance;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.supervised.instance.Resample;
 import weka.filters.unsupervised.attribute.Remove;
+
 /**
  *
- * @author Sekar Anglila/13514069
- *          Diastuti Utami/13514071
- *          Bervianto Leo P/13514047
+ * @author Sekar Anglila/13514069 Diastuti Utami/13514071 Bervianto Leo
+ * P/13514047
  */
 public class Weka_decisionTree {
 
+    private Instances data;
+    private Instances headerData;
+    private Classifier classifier;
+    private Classifier model;
+    private Scanner scan;
+
+    public Weka_decisionTree() {
+        scan = new Scanner(System.in);
+    }
+
     //Reader
-    public Instances readFile(String filename) throws Exception {
+    public void readData(String filename) {
+        try {
+            data = DataSource.read(filename);
+            if (data.classIndex() == -1) {
+                data.setClassIndex(data.numAttributes() - 1);
+            }
+            headerData = new Instances(this.data, 0);
+        } catch (Exception ex) {
+            Logger.getLogger(Weka_decisionTree.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("File tidak dapat dibuka.");
+        }
+    }
+
+    public void readModel(String filename) {
+        try {
+            Object o[] = SerializationHelper.readAll(filename);
+            this.model = (Classifier) o[0];
+            this.headerData = (Instances) o[1];
+        } catch (Exception ex) {
+            Logger.getLogger(Weka_decisionTree.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("File tidak dapat dibuka.");
+        }
+    }
+
+    public void saveModel(String filename) throws Exception {
         //Kamus Lokal
-        Instances data = null;
-        BufferedReader reader;
-
-        //Algoritma
-        reader = new BufferedReader(new FileReader(filename));
-        data = new Instances(reader);
-        data.setClassIndex(data.numAttributes() - 1);
-        reader.close();
-
-        return data;
+        if (this.model != null) {
+            SerializationHelper.write(filename, new Object[]{this.model, this.headerData});
+        } else {
+            System.out.println("Belum ada model. Tidak ada yang disimpan.");
+        }
     }
 
     //Resample and Remove Attributes
-    public Instances filterData(Instances data) throws Exception {
+    public void filterData() throws Exception {
         //Kamus Lokal
         Resample filter = new Resample();
         Instances filterRes;
         Remove r = new Remove();
 
         //Algoritma
-        //r.setAttributeIndices("28");
-        r.setInputFormat(data);
-        filterRes = Filter.useFilter(data, r);
+        r.setInputFormat(this.data);
+        filterRes = Filter.useFilter(this.data, r);
 
-        return filterRes;
+        this.data = filterRes;
     }
 
-    public Classifier skemaTenFolds(Instances data) throws Exception {
+    public void skemaTenFolds() throws Exception {
         //Kamus Lokal
-        J48 dt = new J48();
-        int seed = 1;
-        int folds = 10;
+        System.out.print("Jumlah Fold: ");
+
+        long seed = System.currentTimeMillis();
+        int folds = scan.nextInt();
+        int numInstances = this.data.numInstances();
+        if (folds > numInstances) {
+            folds = numInstances;
+            System.out.println("Folds using maximum instance");
+        } else {
+            folds = 2;
+            System.out.println("Folds using minimum folds.");
+        }
         Random rand = new Random(seed);
-        Instances randData = new Instances(data);
+        Instances randData = new Instances(this.data);
 
         //Algoritma
         randData.randomize(rand);
         //Stratify
-        if (randData.classAttribute().isNominal()){
+        if (randData.classAttribute().isNominal()) {
             randData.stratify(folds);
         }
         Evaluation eval = new Evaluation(randData);
-        for(int n = 0; n < folds; n++){
+        for (int n = 0; n < folds; n++) {
             Instances train = randData.trainCV(folds, n);
             Instances test = randData.testCV(folds, n);
-            dt.buildClassifier(train);
-            eval.evaluateModel(dt, test);
+            this.model.buildClassifier(train);
+            eval.evaluateModel(this.model, test);
         }
         //Menampilkan di Layar
         System.out.println();
@@ -88,67 +125,108 @@ public class Weka_decisionTree {
         System.out.println(eval.toMatrixString());
         System.out.println();
 
-        return dt;
     }
 
-    public Classifier skemaFullTraining(Instances data) throws Exception {
+    public void skemaFullTraining() throws Exception {
         //Kamus Lokal
-        J48 dt = new J48();
-        Evaluation eval = new Evaluation(data);
-
+        Evaluation eval = new Evaluation(this.data);
         //Algoritma
-        dt.buildClassifier(data);
-        eval.evaluateModel(dt, data);
+        this.model.buildClassifier(data);
+        eval.evaluateModel(this.model, data);
         //Menampilkan di Layar
-
         System.out.println();
         System.out.println("====================Results===================");
         System.out.println(eval.toSummaryString());
         System.out.println(eval.toMatrixString());
         System.out.println();
-
-        return dt;
     }
 
-    public Classifier readObject(String filename) throws Exception {
-        //Kamus Lokal
-        Classifier ClassRes;
-        ObjectInputStream reader = new ObjectInputStream(new FileInputStream(filename));
-
-        //Algoritma
-        ClassRes = (Classifier) reader.readObject();
-        reader.close();
-        return ClassRes;
-    }
-
-    public void saveObject(Classifier C, String filename) throws Exception {
-        //Kamus Lokal
-        ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(filename));
-
-        //Algoritma
-        writer.writeObject(C);
-        writer.flush();
-        writer.close();
-    }
-
-    public String classify(Instance data, Classifier C) throws Exception{
-        double result = C.classifyInstance(data);
-        String result_string = data.classAttribute().value((int) result);
+    public String classify(Instance newData) throws Exception {
+        double result = this.model.classifyInstance(newData);
+        String result_string = newData.classAttribute().value((int) result);
 
         return result_string;
     }
 
-    public void Evaluates(Instances dataDisc, Classifier classRes) throws Exception{
+    public void evaluatesModel() throws Exception {
         //Kamus Lokal
-        Evaluation eval = new Evaluation(dataDisc);
+        Evaluation eval = new Evaluation(this.data);
 
         //Algoritma
-        eval.crossValidateModel(classRes, dataDisc, 10, new Random(1));
-        System.out.println(eval.toSummaryString("======================Results======================\n",true));
-        System.out.println(eval.fMeasure(1)+" "+eval.recall(1));
+        eval.crossValidateModel(this.model, this.data, 10, new Random(1));
+        System.out.println(eval.toSummaryString("======================Results======================\n", true));
+        System.out.println(eval.fMeasure(1) + " " + eval.recall(1));
     }
 
-    public int Option(){
+    private void classifyInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void removeAttributes() {
+        // TODO: Ongoing
+    }
+
+    private void showHeaderFile() {
+        //Menampilkan ke layar
+        System.out.println();
+        System.out.print("Header File: \n");
+        System.out.println(this.headerData);
+        System.out.println();
+    }
+
+    private void splitTest() {
+        try {
+            System.out.print("Masukan berapa persen yang akan digunakan: ");
+            int percent = scan.nextInt();
+            if (percent > 0 && percent < 100) {
+                int trainSize = (int) Math.round(this.data.numInstances() * percent / 100.0);
+                int testSize = this.data.numInstances() - trainSize;
+                Instances train = new Instances(this.data, 0, trainSize);
+                Instances test = new Instances(this.data, trainSize, testSize); 
+                Evaluation eval = new Evaluation(train);
+                //Algoritma
+                this.model.buildClassifier(train);
+                eval.evaluateModel(this.model, test);
+                //Menampilkan di Layar
+                System.out.println();
+                System.out.println("====================Results===================");
+                System.out.println(eval.toSummaryString());
+                System.out.println(eval.toMatrixString());
+                System.out.println();
+            } else {
+                System.out.println("Persen harus diantara 0-100");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Weka_decisionTree.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void skemaTestSet() {
+        try {
+            System.out.print("Masukan file test: ");
+            String filename = scan.nextLine();
+            Instances dataTest = DataSource.read(filename);
+            if (dataTest.classIndex() == -1) {
+                dataTest.setClassIndex(dataTest.numAttributes() - 1);
+            }
+            //Kamus Lokal
+            Evaluation eval = new Evaluation(this.data);
+            //Algoritma
+            this.model.buildClassifier(this.data);
+            eval.evaluateModel(this.model, dataTest);
+            //Menampilkan di Layar
+            System.out.println();
+            System.out.println("====================Results===================");
+            System.out.println(eval.toSummaryString());
+            System.out.println(eval.toMatrixString());
+            System.out.println();
+        } catch (Exception ex) {
+            Logger.getLogger(Weka_decisionTree.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("File tidak dapat dibuka.");
+        }
+    }
+
+    public int Option() {
         //Kamus Lokal
         int pil1;
         Scanner input = new Scanner(System.in);
@@ -158,27 +236,55 @@ public class Weka_decisionTree {
         System.out.print("Tentukan apa yang ingin dilakukan: \n");
         System.out.print("1. Skema 10-fold cross validation \n");
         System.out.print("2. Skema Full Training \n");
-        System.out.print("3. Load Model \n");
-        System.out.print("4. Input Instance \n");
-        System.out.print("5. Exit \n");
+        System.out.print("3. Menguji dengan set test \n");
+        System.out.print("4. Percentage split\n");
+        System.out.print("5. Load Model \n");
+        System.out.print("6. Classify Instance \n");
+        System.out.print("7. Remove Attributes \n");
+        System.out.print("8. Exit \n");
         System.out.print("Pilihan anda: ");
         pil1 = input.nextInt();
         return pil1;
+    }
+
+    public void chooseClassifier() {
+        int pilihan;
+        Scanner input = new Scanner(System.in);
+
+        //Algoritma
+        System.out.println();
+        System.out.print("Pilih Classifier: \n");
+        System.out.print("1. myID3 \n");
+        System.out.print("2. myJ48 \n");
+        System.out.print("3. J48 - Weka \n");
+        System.out.print("4. ID3 - Weka \n");
+        System.out.print("Pilihan anda (default ID3 Weka, another 1-4 will choose ID3 Weka) : ");
+        pilihan = input.nextInt();
+
+        switch (pilihan) {
+            case 1:
+                this.model = new MyID3();
+                break;
+            case 2:
+                this.model = new MyJ48();
+                break;
+            case 3:
+                this.model = new J48();
+                break;
+            default:
+                this.model = new ID3();
+                break;
+        }
     }
 
     /**
      * @param args the command line arguments
      * @throws java.lang.Exception
      */
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         //Kamus
         Weka_decisionTree TW = new Weka_decisionTree();
         Scanner input = new Scanner(System.in);
-        String filename = null;
-        String filename2 = null;
-        int pil = 0;
-        Instances data, dataDisc;
-        Classifier classRes = null;
 
         //Algoritma
         System.out.println("-------------------------------------------------------------------\n");
@@ -190,101 +296,82 @@ public class Weka_decisionTree {
         System.out.println();
         System.out.println("Membaca File");
         System.out.print("Masukkan nama file: ");
-        filename = input.nextLine();
-        data = TW.readFile(filename);
+        String filename = input.nextLine();
+        TW.readData(filename);
 
-        //Menampilkan ke layar
-        System.out.println();
-        System.out.print("Header File: \n");
-        System.out.println(new Instances (data, 0));
-        System.out.println();
+        TW.showHeaderFile();
 
         //Discretize
         System.out.println("===============Resample==============");
-        dataDisc = TW.filterData(data);
-        System.out.println(new Instances (dataDisc, 0));
+        TW.filterData();
+
+        TW.showHeaderFile();
+
+        TW.chooseClassifier();
 
         //Pilihan Pengelolaan data
-        pil = TW.Option();
+        int pil = TW.Option();
 
         //Loop pilihan 2
-        while (pil != 5){
-            if (pil == 1){ //Skema Ten Folds
-                //Menampilkan ke layar
-                System.out.println();
-                System.out.print("Header File: \n");
-                System.out.println(new Instances (dataDisc, 0));
-                System.out.println();
-
-                //Skema 10 Folds
-                classRes = TW.skemaTenFolds(dataDisc);
-                System.out.println();
-
-                //Pilihan
-                pil = TW.Option();
-            } else if (pil == 2){ //Skema Full Training
-                //Menampilkan ke layar
-                System.out.println();
-                System.out.print("Header File: \n");
-                System.out.println(new Instances (dataDisc, 0));
-                System.out.println();
-
-                //Skema Full Training
-                classRes = TW.skemaFullTraining(dataDisc);
-                System.out.println();
-
-                //Menyimpan Model
-                System.out.println("===============Menyimpan Model==============");
-                System.out.print("File name: ");
-                filename = input.next();
-                TW.saveObject(classRes, filename);
-                System.out.print("Berhasil disimpan!\n");
-
-                //Pilihan
-                pil = TW.Option();
-            } else if (pil == 3) { //Load File
-                //Load Model
-                System.out.print("Masukkan nama File: ");
-                filename2 = input.next();
-                Classifier cls = TW.readObject(filename2);
-                System.out.print("Berhasil dibaca!\n\n");
-
-                //Evaluate
-                TW.Evaluates(dataDisc, classRes);
-
-                //Pilihan
-                System.out.println();
-                pil = TW.Option();
-            } else if (pil == 4){ //Add Instance
-                //input instance
-                System.out.println();
-                classRes.buildClassifier(data);
-                Instance insert = new DenseInstance(4);
-                insert.setDataset(data);
-                for(int i=0; i<data.classIndex(); i++){
-                    System.out.print("Atribut ke-" + i+1+"= ");
-                    float atribut = input.nextFloat();
-                    insert.setValue(i,atribut);
-                }
-
-                //classify
-                String baru = TW.classify(insert, classRes);
-                System.out.println("Hasil Klasifikasi: " + baru);
-
-                //Pilihan
-                System.out.println();
-                pil = TW.Option();
-            } else {
-                //Error Message
-                System.out.println("Pilihan tidak valid!\n");
-
-                //Pilihan Pengelolaan data
-                pil = TW.Option();
+        while (pil != 6) {
+            switch (pil) {
+                case 1:
+                    //Skema CV 10 Folds
+                    TW.showHeaderFile();
+                    TW.skemaTenFolds();
+                    TW.saveModel("cv-model.model");
+                    break;
+                case 2:
+                    //Skema Full Training
+                    //Menampilkan ke layar
+                    TW.showHeaderFile();
+                    //Skema Full Training
+                    TW.skemaFullTraining();
+                    TW.saveModel("full-model.model");
+                    //Pilihan
+                    break;
+                case 3:
+                    // Test Set
+                    TW.showHeaderFile();
+                    TW.skemaTestSet();
+                    TW.saveModel("test-set-model.model");
+                    break;
+                case 4:
+                    // Split Test
+                    TW.showHeaderFile();
+                    TW.splitTest();
+                    TW.saveModel("split.model");
+                    break;
+                case 5:
+                    //Load File
+                    //Load Model
+                    System.out.print("Masukkan nama File: ");
+                    String filename2 = input.nextLine();
+                    TW.readModel(filename2);
+                    System.out.print("Berhasil dibaca!\n\n");
+                    //Evaluate
+                    TW.evaluatesModel();
+                    //Pilihan
+                    System.out.println();
+                    break;
+                case 6:
+                    //Clasify one unseen data
+                    TW.classifyInstance();
+                    break;
+                case 7:
+                    // Remove Attribute
+                    TW.removeAttributes();
+                    break;
+                default:
+                    //Error Message
+                    System.out.println("Pilihan tidak valid!\n");
+                    break;
             }
+            TW.chooseClassifier();
+            pil = TW.Option();
         }
         //Exit
         System.out.println();
         System.out.println("Terima Kasih!\n");
     }
-    
 }
